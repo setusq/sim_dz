446cc917947b3ecc510274e98a10bad1ace7687d
tsukerin <setusq@gmail.com>
2023-09-13 20:43:37 +0300

    Update график_2_задачи.py

--- график_2_задачи.py
+++ график_2_задачи.py
@@ -10,43 +10,23 @@ plt.ylabel('x2')
 plt.xlim(-5,15)
 plt.ylim(-5,15)
 
-plt.plot(x1, (21 - 2*x1) / 3, label='2X1+3X2<=21', color='salmon', linestyle='dashed')
-plt.plot(x1, 10 - x1, label='X1+X2<=10', color='pink', linestyle='dashed')
-plt.plot(x1, (16 - 2 * x1) / 2, label='2X1+2X2<=16', color='#DB7093', linestyle='dashed')
+plt.plot(x1, (30 - 5*x1) / 3, label='5x+3y>=30', color='salmon', linestyle='dashed')
+plt.plot(x1, x1-3, label='x-y<=3', color='pink', linestyle='dashed')
+plt.plot(x1, (15 + 3 * x1) / 5, label='-3x+5y<=15', color='#DB7093', linestyle='dashed')
 plt.axvline(x=0, color='gray', linestyle='dashed')
 plt.axhline(y=0, color='gray', linestyle='dashed')
 
-plt.fill_between([0, 0, 3,8], [0, 7, 5, 0], color='lavender')
+#plt.fill_between([0, 0, 3,8], [0, 7, 5, 0], color='lavender')
 
-plt.arrow(0,0,  2.7,  2, head_width=0.4, head_length=0.8, fc='black', ec='black', label='вектор z')
+plt.arrow(0,0 ,-1,2, head_width=0.3, head_length=0.6, fc='black', ec='black', label='вектор z')
 x1_green = np.linspace(-3, 3, 100)
-plt.plot(x1_green, -3/4 * x1_green, linestyle='-.', color='green', label='перпендикуляр')
+plt.plot(x1_green, 1/2 * x1_green, linestyle='-.', color='green', label='перпендикуляр')
 
 plt.plot(8,0,'o', c='#DB7093')
 plt.text(8,-1, 'max (8,0)')
-plt.title('Задание 1')
+plt.title('Задание 2')
 plt.grid(True)
 plt.legend()
 plt.show()
 
-#решение с помощью библиотеки pulp
-import pulp
-import time
-start = time.time()
-x1 = pulp.LpVariable("x1", lowBound=0)
-x2 = pulp.LpVariable("x2", lowBound=0)
-problem = pulp.LpProblem('0',pulp.LpMaximize)
-problem += 400*x1 + 300*x2, "Функция цели"
-problem += 2*x1+ 3*x2 <= 21, "1"
-problem +=x1+x2<=10, "2"
-problem +=2*x1+2*x2<=16, "3"
-problem.solve()
-print ("Результат:")
-for variable in problem.variables():
-    print (variable.name, "=", variable.varValue)
-print ("Прибыль:")
-#print (value(problem.objective))
-stop = time.time()
-print ("Время :")
-print(stop- start)
 
